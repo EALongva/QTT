e074cb533b778bfdc14cad2144156ed4507ee6b3
EALongva <e.a.longva@fys.uio.no>
2022-04-05 05:12:23 +0200

    multiprocess test

--- utils/paratest.py
+++ utils/paratest.py
@@ -1,8 +1,12 @@
 # testing multiprocessing
 
+from lib2to3.pgen2 import grammar
 import os
 import numpy as np
 import multiprocessing as mp
+
+
+"""
 
 def info(title):
     print(title)
@@ -14,13 +18,35 @@ def f(name):
     info('function f')
     print('hello', name)
 
+def g(var):
+    gArray = np.zeros(var)
+    for i in range(var):
+        gArray[i] = i
+    print('hello')
 
 if __name__ == '__main__':
     info('main line')
-    p = mp.Process(target=f, args=('bob',))
+    var = 10
+    p = mp.Process(target=g, args=(var,))
     p.start()
     p.join()
 
+"""
+
+
+
+
+
+def foo(q):
+    q.put('hello')
+
+if __name__ == '__main__':
+    ctx = mp.get_context('spawn')
+    q = ctx.Queue()
+    p = ctx.Process(target=foo, args=(q,))
+    p.start()
+    print(q.get())
+    p.join()
 
 
 #print(os.getppid())
