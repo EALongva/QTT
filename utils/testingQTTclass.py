61ecda60aa9b05ab176b4045fdfea8a476422cc8
EALongva <e.a.longva@fys.uio.no>
2022-05-02 02:27:13 +0200

    parallel code update

--- utils/testingQTTclass.py
+++ utils/testingQTTclass.py
@@ -1,9 +1,70 @@
 # testingQTTclass
 
 from QTT import *
+import multiprocessing as mp
+from tqdm import tqdm #love this
 
-def MCsimulation(S, N, delta, epsilon):
+def f(S, N, seed):
 
-    x=2
+    print(seed)
+    rnd.seed(seed)
+
+    MC_traj = np.zeros((S, N, 2, 1), dtype='complex128')
+
+    for s in range(S):
+
+        traj_result = np.zeros((N, 2, 1), dtype='complex128')
+
+        for i in range(N):
+
+            p = rnd.random()
+            time.sleep(0.001)
+
+            if p >= 0.5:
+
+                traj_result[i] = bas0
+
+            else:
+
+                traj_result[i] = bas1
+
+        MC_traj[s] = traj_result
+
+    return MC_traj
+
+def mpTest(S, N, seed, ncpu):
+
+    #ncpu = number of CPUs to use
+
+    args = []
+
+    for i in range(ncpu):
+        dseed = seed + i
+        args.append([S, N, dseed])
+
+    pool = mp.Pool(ncpu)
+    result = pool.starmap(f, args)
+    pool.close()
+
+    avgres = np.mean(result, axis=(0,1))
+
+    print(avgres)
 
     return 0
+
+# run
+
+ncpu = 4
+S = int(ncpu * 200)
+N = 3
+seed = 200
+
+#test = f(S, N, seed)
+#print(test)
+
+start = time.perf_counter()
+mpTest(S, N, seed, ncpu)
+fin = time.perf_counter()
+
+print('time: ', fin - start)
+
